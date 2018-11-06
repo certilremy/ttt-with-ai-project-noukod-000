1d5e01ecf3d4f7fa29e7adf7d45c5b3bcccd666d
Certil Remy <certil@noukod.com>
2018-11-06 18:16:36 +0000

    automatically backed up by learn

---
+++ lib/players/human.rb
@@ -0,0 +1,17 @@
+module Players
+
+  class Human < Player
+
+    def move(board)
+
+      puts "Where do you want to play?"
+
+      input=gets.strip
+
+      return input
+
+    end
+
+  end
+
+end
